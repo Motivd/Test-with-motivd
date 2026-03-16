4593fe60b98847a5b6b613381a0ebc44e93a0cbb
Elijah Royaie <79429824+VFitter@users.noreply.github.com>
2026-03-16 00:43:58 -0400

    Update pages/index.js (Motivd push)

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function HomePage() {
+  return (
+    <div>
+      <Head>
+        <title>TODOODO App</title>
+      </Head>
+      <h1>Welcome to the TODOODO App</h1>
+    </div>
+  );
+}
+
+export default HomePage;
