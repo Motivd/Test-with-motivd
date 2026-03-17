aa44bd5b722aa97e7ddb6546eb251d528a9a6fbb
Elijah Royaie <79429824+VFitter@users.noreply.github.com>
2026-03-17 02:29:33 -0400

    Update pages/contact.js (Motivd push)

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Email: contact@todoxodo.com</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
+
+export default Contact;
