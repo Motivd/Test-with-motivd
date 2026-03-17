0e2c67cc7237b414eb1f510b4f82c088c3a34437
Elijah Royaie <79429824+VFitter@users.noreply.github.com>
2026-03-17 02:29:33 -0400

    Update pages/about.js (Motivd push)

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function About() {
+  return (
+    <div>
+      <h1>This is the about page</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
+
+export default About;
