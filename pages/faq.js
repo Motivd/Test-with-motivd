f2cbef9bbc4bc7fce163b37eeeef0985d166cd3d
Elijah Royaie <79429824+VFitter@users.noreply.github.com>
2026-03-17 02:29:34 -0400

    Update pages/faq.js (Motivd push)

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function FAQ() {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
+
+export default FAQ;
